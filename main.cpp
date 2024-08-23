3e49d07f33e906bb9a3a2b1aa6fa60384996590a
RoMa <102636132+Dev-RoMa@users.noreply.github.com>
2024-08-23 18:35:40 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,27 @@ using namespace sf;
 //window size
 int width = 1024;
 int height = 768;
+int roadW = 2000;
+int segL = 200;
+float camD = 0.9;
 
+struct Line
+{
+    float x,y,z;
+    float X,Y,W;
+    float scale;
+
+    Line() {x,y,z=0;}
+
+    //from world to screen
+    void project(int camX, int camY,int camZ)
+    {
+        scale = camD/(z-camZ);
+        X = (1 + scale*(x - camX)) * width/2;
+        Y = (1 - scale*(y - camY)) * height/2;
+        W = scale * roadW * width/2;
+    }
+};
 
 //this draws the shape
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2)
@@ -25,6 +45,21 @@ int main()
     RenderWindow app(VideoMode(width,height), "Racing!");
     app.setFramerateLimit(60);
 
+    std::vector<Line> lines;
+
+    for (int i = 0; i<1600;i++)
+    {
+        Line line;
+        line.z =i*segL;
+
+        lines.push_back(line);
+    }
+
+    int N = lines.size();
+    int pos = 0;
+    int playerX = 0;
+
+
     while(app.isOpen())
     {
         Event e;
@@ -33,8 +68,31 @@ int main()
             if(e.type == Event::Closed)
             app.close();
         }
+
+    if (Keyboard::isKeyPressed(Keyboard::Up)) pos +=200;
+    if (Keyboard::isKeyPressed(Keyboard::Down)) pos -=200;
+    if (Keyboard::isKeyPressed(Keyboard::Right)) playerX +=200;
+    if (Keyboard::isKeyPressed(Keyboard::Left)) playerX -=200;
+
     app.clear();
-    drawQuad(app, Color::Red, 500,500,200,500,300,100);
+    int startPos = pos/segL;
+
+    //draw road//
+    for(int n=startPos; n<startPos+300;n++)
+    {
+        Line &l = lines[n%N];
+        l.project(playerX, 1500,pos-5);
+
+        Color grass = (n/3)%2?Color(16,200,16):Color(0,154,0);
+        Color rumble = (n/3)%2?Color(255,255,255):Color(0,0,0);
+        Color road = (n/3)%2?Color(107,107,107):Color(105,105,105);
+
+        Line p = lines[(n-1)%N];
+
+        drawQuad(app, grass, 0, p.Y, width,0, l.Y, width);
+        drawQuad(app, rumble, p.X, p.Y, p.W*1.2,l.X, l.Y, l.W*1.2);
+        drawQuad(app,road, p.X,p.Y,p.W,l.X,l.Y,l.W);
+    }
     app.display();
     }
     return 0;
